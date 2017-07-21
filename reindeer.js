fda2ab93c1e90f75831a039aa05172e4baa2e036
Jasmine Quach <xjasminequach@gmail.com>
2017-07-20 22:33:32 -0500

    initial commit

---
+++ reindeer.js
@@ -0,0 +1,9 @@
+var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
+var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
+
+var hohohoElement = document.getElementById("reindeer");
+for (let i = 0; i < reindeer.length; i++) {
+  let reindeerName = colors[i] + " " + reindeer[i] + ", ";
+  console.log(reindeerName);
+  hohohoElement.innerHTML += reindeerName;
+}
